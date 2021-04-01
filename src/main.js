15b7aed045c1bb67a3ac9a3b381f1eb7c7fdc600
shiu <wsh9075@163.com>
2021-04-02 00:22:25 +0800

    添加全局颜色和样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import useElement from './element-views'
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/style.css'
 useElement();
 Vue.config.productionTip = false
 new Vue({
