1e0c16ed1f413cb0f54b0231fed0883b0db68d4e
shiu <wsh9075@163.com>
2021-04-02 00:22:51 +0800

    添加头部模板

---
+++ src/components/layout/header.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+        <div class="logo">
+            <img src="../../assets/img/logo.svg" alt=""/>
+        </div>
+    </nav>
+</template>
+<style lang="scss" scoped>
+    .logo img{
+        width:60px;
+        height:60px;
+        margin-top:10px;
+    }
+</style>
